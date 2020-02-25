43ed2ba8b0c04a395b1f3f96101dbbf871f3e4bf
Yao Yu <yao97ming@gmail.com>
2020-02-24 23:28:28 -0800

    able to view movie_id results

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -30,6 +30,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 
             self.movies = dataDictionary["results"] as! [[String:Any]]
+            print(dataDictionary)
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
               // TODO: Reload your table view data
--- Flix/TrailerViewController.swift
+++ Flix/TrailerViewController.swift
@@ -23,17 +23,12 @@ class TrailerViewController: UIViewController, WKUIDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*if let result = movie["results"] as? [[String: Any]] {
-            let id = result[0]
-            let key = id["key"]
-              let myURL = URL(string:"https://www.youtube.com/watch?v=\(key)")
-                     let myRequest = URLRequest(url: myURL!)
-                     webView.load(myRequest)
-        }*/
-        let id = movie["id"]
-        print(id)
+
+        let id = movie["id"] as! CVarArg
+        let stringID = String(format: "%@", id)
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/\(id)/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/\(stringID)/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
                let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
                let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
                let task = session.dataTask(with: request) { (data, response, error) in
@@ -42,8 +37,8 @@ class TrailerViewController: UIViewController, WKUIDelegate {
                      print(error.localizedDescription)
                   } else if let data = data {
                      let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                    let idDictionary = dataDictionary["id"] as! [String: Any]
-                   self.movies = idDictionary["results"] as! [[String:Any]]
+
+                   self.movies = dataDictionary["results"] as! [[String:Any]]
                      // TODO: Get the array of movies
                      // TODO: Store the movies in a property to use elsewhere
                      // TODO: Reload your table view data
@@ -52,13 +47,20 @@ class TrailerViewController: UIViewController, WKUIDelegate {
                }
                task.resume()
         
-      //  let myURL = URL(string:"https://www.youtube.com/watch?v=\(key)")
-             //  let myRequest = URLRequest(url: myURL!)
-             //  webView.load(myRequest)
+         let key = movies["key"] as! [[String: Any]]
+         print(key)
+        /*
+        let stringKey = String(format: "%@", key)
         
+        
+       let myURL = URL(string:"https://www.youtube.com/watch?v=\(stringKey)")
+               let myRequest = URLRequest(url: myURL!)
+               webView.load(myRequest)
+        */
        //print(movie["id"])
         
     }
+    
     
 
     /*
